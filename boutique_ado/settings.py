7f9f40c0c1ffa486f2f772c87403ec250095a96f
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2021-07-03 16:54:34 +0000

    Added home app and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',  # used by socials for callback
     'allauth',  # allauth itself
     'allauth.account',  # user accounts (reg/log)
-    'allauth.socialaccount'  # social media accounts
+    'allauth.socialaccount',  # social media accounts
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +61,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
