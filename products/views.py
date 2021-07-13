984cad334880b50a6b568eef7b2ac691998b6f68
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2021-07-13 20:13:40 +0000

    Added sorting

--- products/views.py
+++ products/views.py
@@ -15,11 +15,35 @@ def all_products(request):
     """
 
     products = Product.objects.all()
+
+    # set to default (none)
     query = None
-    # set query to default (none)
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+
+            # sortkey preserves original field 'name'
+            if sortkey == 'name':
+                # if user is sorting by name
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                # if user is sorting in decending
+                direction = request.GET['direction']
+
+                # decending = '-'
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            
+            # actually sort the product
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -36,6 +60,9 @@ def all_products(request):
             # 'icontains' specific class contains query
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+    
+    # return current sorting method to template
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
