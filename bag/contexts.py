e8b25dfa537e4e21ceaf6cdf00c6b5d28a05bacf
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2021-07-14 20:31:28 +0000

    Added quantity boxes to shopping bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -48,7 +48,7 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
